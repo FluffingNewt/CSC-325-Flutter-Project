2f65dc9d36226ed0b907d293dd20531c8686622d
Davis Guest <11026301@live.mercer.edu>
2024-03-19 08:06:41 +0000

    created starter smoke tests

--- wordle/lib/app/app.dart
+++ wordle/lib/app/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Wordle',
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: const Color.fromARGB(255, 36, 36, 36)),
       home: const HomeScreen(),
     );
